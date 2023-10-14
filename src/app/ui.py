18ecbc5425b94b4acaaf4c6a780a085d06d3e678
Odonata971 <130738441+Odonata971@users.noreply.github.com>
2023-10-14 12:42:44 +0200

    Update ui : change appearance to make it better
    Some more work has to be done

--- src/app/ui.py
+++ src/app/ui.py
@@ -7,24 +7,71 @@ class App(CTk):
         super().__init__()
         self.geometry("600x450")
         self.title("QuikAccessHub")
+        self.grid_rowconfigure(3, weight=1)  # configure grid system
+        self.grid_columnconfigure(0, weight=1)
+
+        self.title = TitleFrame(master=self, fg_color="#FFFFFF")
+        self.title.grid(row=0, column=0, padx=20, pady=10, sticky="nsew")
+
+        self.templates = TemplatesFrame(master=self, fg_color="#FFFFFF")
+        self.templates.grid(row=1, column=0, padx=20, pady=0, sticky="nsew")
+
+        self.configure_templates = ConfigureTemplates(master=self, fg_color="#FFFFFF")
+        self.configure_templates.grid(row=2, column=0, padx=20, pady=10, sticky="nsew")
+
+
+class TemplatesFrame(CTkFrame):
+    def __init__(self, master, **kwargs):
+        super().__init__(master, **kwargs)
+        self.grid_rowconfigure(6, weight=1)  # configure grid system
+        self.grid_columnconfigure(0, weight=1)
+
         templates = ["Work", "Anime", "Games", "Dev", "Music"]
+        # add widgets onto the frame, for example:
+        self.label = CTkLabel(self, text="Choose a template", font=CTkFont(family="Bahnschrift", size=20),
+                              text_color="#353535")
 
-        self.titre = CTkLabel(self, text="QuikAccessHub", font=("Arial", 30))
-        self.titre.pack(padx=5, pady=5)
+        self.template_01 = CTkButton(self, text=templates[0], command=lambda: launch_applications(templates[0]),
+                                     font=CTkFont(family="Bahnschrift", size=15))
+        self.template_02 = CTkButton(self, text=templates[1], command=lambda: launch_applications(templates[1]),
+                                     font=CTkFont(family="Bahnschrift", size=15))
+        self.template_03 = CTkButton(self, text=templates[2], command=lambda: launch_applications(templates[2]),
+                                     font=CTkFont(family="Bahnschrift", size=15))
+        self.template_04 = CTkButton(self, text=templates[3], command=lambda: launch_applications(templates[3]),
+                                     font=CTkFont(family="Bahnschrift", size=15))
+        self.template_05 = CTkButton(self, text=templates[4], command=lambda: launch_applications(templates[4]),
+                                     font=CTkFont(family="Bahnschrift", size=15))
 
-        self.parameters = CTkButton(self,text="parameters")
-        self.parameters.pack(padx=5, pady=5)
-        self.template_01 = CTkButton(self, text=templates[0], command=lambda: launch_applications(templates[0]))
-        self.template_02 = CTkButton(self, text=templates[1], command=lambda: launch_applications(templates[1]))
-        self.template_03 = CTkButton(self, text=templates[2], command=lambda: launch_applications(templates[2]))
-        self.template_04 = CTkButton(self, text=templates[3], command=lambda: launch_applications(templates[3]))
-        self.template_05 = CTkButton(self, text=templates[4], command=lambda: launch_applications(templates[4]))
-        self.template_01.pack(padx=5, pady=5)
-        self.template_02.pack(padx=5, pady=5)
-        self.template_03.pack(padx=5, pady=5)
-        self.template_04.pack(padx=5, pady=5)
-        self.template_05.pack(padx=5, pady=5)
+        self.label.grid(row=0, column=0, padx=20, pady=15)
+        self.template_01.grid(row=1, column=0, padx=20, pady=5)
+        self.template_02.grid(row=2, column=0, padx=20, pady=5)
+        self.template_03.grid(row=3, column=0, padx=20, pady=5)
+        self.template_04.grid(row=4, column=0, padx=20, pady=5)
+        self.template_05.grid(row=5, column=0, padx=20, pady=5)
+
+
+class TitleFrame(CTkFrame):
+    def __init__(self, master, **kwargs):
+        super().__init__(master, **kwargs)
+        self.grid_rowconfigure(0, weight=1)  # configure grid system
+        self.grid_columnconfigure(0, weight=1)
+        self.title = CTkLabel(self, text="QuikAccessHub", font=CTkFont(family="Bahnschrift", size=40, weight="bold"),
+                              text_color="#353535")
+        self.title.grid(row=0, column=0, padx=5, pady=5)
+
+
+class ConfigureTemplates(CTkFrame):
+    def __init__(self, master, **kwargs):
+        super().__init__(master, **kwargs)
+        self.grid_rowconfigure(0, weight=1)  # configure grid system
+        self.grid_columnconfigure(2, weight=1)
+        self.configure_templates = CTkButton(self, text="Configure Templates", command=print("ok"),
+                                             font=CTkFont(family="Bahnschrift", size=15))
+        self.help = CTkButton(self, text="Help", command=print("help"), font=CTkFont(family="Bahnschrift", size=15))
+        self.configure_templates.grid(row=0, column=0, padx=70, pady=10)
+        self.help.grid(row=0, column=1, padx=45, pady=10)
 
 
 app = App()
+app.configure(fg_color="#D9D9D9")
 app.mainloop()
