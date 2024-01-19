c8cff77230e939099b475cd5ecf33de0a29e8d2a
François SP <francois-sp@gmx.fr>
2024-01-19 20:01:52 +0100

    Give user a feedback when editing a path

--- src/app/ui_config_page.py
+++ src/app/ui_config_page.py
@@ -1,5 +1,5 @@
 from os.path import basename
-from tkinter import PhotoImage
+import tkinter as tk
 from customtkinter import *
 from template_service import *
 from os import path
@@ -26,36 +26,33 @@ class App(CTkFrame):
     def __init__(self, master, app_path: str, tab_url=None, **kwargs):
         super().__init__(master, **kwargs)
         self.tab_url = tab_url
-        self.app_path = app_path
-        self.app_path_SV: StringVar = StringVar()
+        self.app_path_SV: StringVar = tk.StringVar(self, app_path)
+        self.app_path_SV.trace("w", self.update_app_path_label)
 
         self.grid_columnconfigure(0, weight=1)  # configure grid system
 
-        self.label_path_app = CTkLabel(self, text=get_application_name(app_path), fg_color="transparent",
-                                       text_color="#000")
-        self.entry_path_app = CTkEntry(self, placeholder_text="Chemin vers l'executable du navigateur",
-                                       text_color="#FFFFFF", textvariable=self.app_path_SV)
-        self.explorer_button = CTkButton(self, text="Explorer", command=lambda: self.open_explorer())
-        self.entry_path_app.insert(0, app_path)
-
-        if not path.exists(app_path):  # If the path doesn't exist we change the color of the text
-            self.label_path_app.configure(text_color="#FF0000")
-            self.entry_path_app.configure(text_color="#FF0000")
-        self.app_path_SV.trace("w", self.update())
+        self.label_path_app = CTkLabel(self, fg_color="transparent", text_color="#000")
+        self.entry_path_app = CTkEntry(self, text_color="#FFFFFF", textvariable=self.app_path_SV)
+        self.explorer_button = CTkButton(self, text="Explorer", command=self.open_explorer)
 
         self.label_path_app.grid(row=1, column=0, padx=(10, 2), sticky="w")
         self.entry_path_app.grid(row=2, column=0, padx=10, pady=(0, 20), sticky="ew")
         self.explorer_button.grid(row=2, column=1, padx=10, pady=(0, 20), sticky="ew")
 
+        self.update_app_path_label()
+
         if tab_url is not None:
             self.entry_url: list[CTkEntry] = []
             self.label_url: list[CTkLabel] = []
+            self.url_SV: list[StringVar] = []
 
             for i in range(len(tab_url)):
+                self.url_SV.append(tk.StringVar(self, tab_url[i]))
+                self.url_SV[i].trace("w", self.update_url_label)
                 self.label_url.append(CTkLabel(self, text="Url", fg_color="transparent", text_color="#000"))
-                self.entry_url.append(CTkEntry(self, text_color="#FFFFFF"))
+                self.entry_url.append(CTkEntry(self, text_color="#FFFFFF", textvariable=self.url_SV[i]))
 
-                self.entry_url[i].insert(0, tab_url[i])
+                # self.entry_url[i].insert(0, tab_url[i])
 
                 self.label_url[i].grid(row=2 * i + 3, column=0, padx=10, sticky="w")
                 bottom_pading = 0
@@ -63,39 +60,64 @@ class App(CTkFrame):
                     bottom_pading = 10
                 self.entry_url[i].grid(row=2 * i + 4, column=0, padx=10, pady=(0, bottom_pading), sticky="ew")
 
+                self.update_url_label(i)
+
             # TODO Add a button to add a new url
 
     def get_data(self) -> dict:
-        data = {"path": self.entry_path_app.get()}
+        """
+        Get the data of the app
+        :return: a dict with the path to the executable and the list of urls (if it's a browser)
+        ex:
+        {
+            "path": "C:/Program Files/Google/Chrome/chrome.exe",
+            "urls": ["https://www.google.com", "https://www.youtube.com"]
+        }
+        """
+        data = {"path": self.app_path_SV.get()}
         if self.tab_url is not None:
             data["urls"] = [url.get() for url in self.entry_url]
         return data
 
     def open_explorer(self):
         """
-        Open the explorer to choose a browser executable
+        Open the explorer to choose a browser executable.
+        If the user choose a file, the entry is updated with the path to the executable and we update the color
         """
         file_path = filedialog.askopenfilename(initialdir="/", title="Selectionner un executable",
                                                filetypes=(("executables", "*.exe"), ("all files", "*.*")))
         if file_path != "":
-            self.entry_path_app.delete(0, END)
-            self.entry_path_app.insert(0, file_path)
-            if not path.exists(file_path):
-                self.label_path_app.configure(text_color="#FF0000")
-                self.entry_path_app.configure(text_color="#FF0000")
-            else:
-                self.label_path_app.configure(text_color="#000")
-                self.entry_path_app.configure(text_color="#FFFFFF")
+            self.app_path_SV.set(file_path)
+        self.update_app_path_label()
 
     def __str__(self):
         if self.entry_path_app.get() == "":
             return "App (empty)"
         return "App " + get_application_name(self.entry_path_app.get())
 
-    def update(self):
-        self.app_path = self.entry_path_app.get()
-        self.label_path_app.configure(text=get_application_name(self.app_path))
-        if not path.exists(self.app_path):
+    def update_app_path_label(self, *args):
+        """
+        Update the label and the entry color with the entry value
+        """
+        app_path = self.app_path_SV.get()
+        self.label_path_app.configure(text=get_application_name(app_path))
+        self.update_color(app_path)
+
+    def update_url_label(self, *args):
+        """
+        Update the label and the entry color with the entry value
+        """
+        for i in range(len(self.entry_url)):
+            url_path = self.url_SV[i].get()
+            self.label_url[i].configure(text=get_site_name(url_path))
+
+    def update_color(self, app_path):
+        """
+        Update the label and the entry color with the entry value.
+        If the path doesn't exist, the color is red.
+        :param app_path: the path to the executable
+        """
+        if not path.exists(app_path):
             self.label_path_app.configure(text_color="#FF0000")
             self.entry_path_app.configure(text_color="#FF0000")
         else:
@@ -111,6 +133,11 @@ def add_app(scroll: CTkScrollableFrame, app: list[App | None]):
 
 
 def show_template(template_name: str):
+    """
+    Show the template in a new window
+    Each app is a frame with a label and an entry where the user can modify the path to the executable
+    :param template_name: the name of the template to show
+    """
     print("Configuring template : " + template_name)
     # Get the template data
     template_data: list[dict] = get_info_template(template_name)
@@ -143,52 +170,76 @@ def show_template(template_name: str):
         else:
             app.append(App(master=scroll, app_path=template_data[i]["path"], fg_color="#FFFFFF"))
 
-        app[i].grid(row=i, column=0, padx=20, pady=10, sticky="new")
-
-
+        app[i].grid(row=i, column=0, padx=0, pady=10, sticky="new")
 
     window.mainloop()
 
 
 def config_page():
+    """
+    Show the config page
+    It's a ist of buttons, each button is a template and when clicked, it shows the template in a new window
+    """
     window = CTk()
     window.configure(fg_color="#D9D9D9")
-    window.title("QuikAccessHub - Config Page")
-    window.title = TitleFrame(master=window, fg_color="#FFFFFF", title="Config Page")
+    window.title("QuikAccessHub - Configuration")
+    window.title = TitleFrame(master=window, fg_color="#FFFFFF", title="Configuration")
     window.title.grid(row=0, column=0, padx=20, pady=10, sticky="nsew")
 
     window.geometry("400x300")
-    window.resizable(False, False)
+    window.resizable(False, True)
 
     window.grid_columnconfigure(0, weight=1)  # configure grid system
+    window.grid_rowconfigure(2, weight=1)
 
     # Get the list of templates
     templates = get_template_list().keys()
     templates_button = []  # list of buttons for each template
 
     if len(templates) == 0:
-        label = CTkLabel(window, text="No template found", font=CTkFont(family="Bahnschrift", size=20),
-                         text_color="#353535")
+        label = CTkLabel(window, text="Aucun template", font=CTkFont(family="Bahnschrift", size=20),
+                         text_color="#353535", fg_color="#FFFFFF")
         label.grid(row=1, column=0, padx=20, pady=15)
+    else:
+        label = CTkLabel(window, text="Choisissez un template à configurer", corner_radius=7,
+                         font=CTkFont(family="Bahnschrift", size=20), text_color="#353535", fg_color="#FFFFFF")
+        label.grid(row=1, column=0, padx=20, pady=5, sticky="nsew")
+
+    scroll = CTkScrollableFrame(master=window, fg_color="#FFFFFF")
+    scroll.grid_columnconfigure(0, weight=1)
+    scroll.grid(row=2, column=0, padx=20,pady=5, sticky="nsew")
 
     # Create a button for each template
     for template in templates:
         # To avoid the problem of the button always taking the last value of template,
         # we use a lambda function with a default argument
-        templates_button.append(CTkButton(window, text=template,
+        templates_button.append(CTkButton(scroll, text=template,
                                           command=lambda template_used=template: show_template(template_used),
                                           font=CTkFont(family="Bahnschrift", size=15)))
 
     # Grid the buttons
     for i in range(len(templates_button)):
-        templates_button[i].grid(row=i + 1, column=0, padx=20, pady=10, sticky="nsew")
+        templates_button[i].grid(row=i, column=0, padx=20, pady=10, sticky="nsew")
 
     window.mainloop()
 
 
 def get_application_name(executable_path: str) -> str:
+    """
+    Get the name of the application from the executable path
+    :param executable_path: the path to the executable (ex: C:/Program Files/Google/Chrome/chrome.exe)
+    :return: the name of the application (ex: chrome)
+    """
     return os.path.splitext(os.path.basename(executable_path))[0]
 
+
+def get_site_name(url: str) -> str:
+    """
+    Get the name of the site from the url.
+    :param url: the url of the site (ex: https://www.google.com)
+    :return: the name of the site (ex: google)
+    """
+    return url.split("/")[2].split(".")[0]
 
 
 def save_template(template_name: str, app: list[App | None], window: CTk):
