1bbb5e6220f3f20f8bc6adc147aec9d75df14ddd
François SP <francois-sp@gmx.fr>
2024-03-27 23:05:38 +0100

    Utilisation d'un texte area pour saisir les urls

--- src/app/ui_config_template.py
+++ src/app/ui_config_template.py
@@ -11,6 +11,10 @@ CHANGE_MADE: bool = False
 
 
 class ConfigurationWindow(CTk):
+    """
+    The window to configure a template
+    """
+
     def __init__(self, template_name, **kwargs):
         super().__init__(**kwargs)
         self.no_app = None
@@ -101,12 +105,14 @@ class ConfigurationWindow(CTk):
             self.app.pop(index)
             self.show_app()
 
-
 class ApplicationFrame(CTkFrame):
+    """
+    A frame to configure an application
+    """
 
-    def __init__(self, master, app_path: str, tab_url=None, **kwargs):
+    def __init__(self, master, app_path: str, tab_url=[], **kwargs):
         super().__init__(master, **kwargs)
-        self.tab_url = tab_url
+
         self.app_path_SV: StringVar = tk.StringVar(self, app_path)
         self.app_path_SV.trace("w", self.update_app_path_label)
 
@@ -122,41 +128,14 @@ class ApplicationFrame(CTkFrame):
 
         self.update_app_path_label()
 
-        if tab_url is not None:
-            self.url_SV: list[StringVar] = []
-            self.label_url: list[CTkLabel] = []
-            self.btn_delete_url: list[CTkButton] = []
-            self.entry_url: list[CTkEntry] = []
+        self.entry_url = None
+        self.url_internet: list[str] = tab_url
 
-            self.create_url_entry(tab_url)
-
-        # TODO Add a button to add a new url
-
-    def create_url_entry(self, tab_url):
-        for i in range(len(tab_url)):
-            self.url_SV.append(tk.StringVar(self, tab_url[i]))
-            self.url_SV[i].trace("w", self.update_url_label)
-
-            self.label_url.append(CTkLabel(self, text="Url", fg_color="transparent", text_color="#000"))
-            self.btn_delete_url.append(CTkButton(self, text="X", fg_color="#F00", width=1,
-                                                 command=lambda index=i: self.delete_url(index), hover_color="#C00"))
-            self.entry_url.append(CTkEntry(self, text_color="#FFF", textvariable=self.url_SV[i]))
-
-            self.update_url_label(i)
-        self.show_urls()
-
-    def show_urls(self):
-        """
-        Show the list of url correctly based of the list of StringVar
-        :return:
-        """
-        for i in range(len(self.url_SV)):
-            self.label_url[i].grid(row=2 * i + 3, column=1, padx=10, sticky="w")
-            bottom_pading = 0
-            if i == len(self.url_SV) - 1:  # If it's the last url we add a bottom padding
-                bottom_pading = 10
-            self.btn_delete_url[i].grid(row=2 * i + 4, column=0, padx=10, pady=(0, bottom_pading), sticky="ew")
-            self.entry_url[i].grid(row=2 * i + 4, column=1, padx=10, pady=(0, bottom_pading), sticky="ew")
+        if len(self.url_internet) != 0:
+            self.add_url()
+        else:
+            self.add_entry_url = CTkButton(self, text="Add an url", command=lambda: self.add_url(from_user=True))
+            self.add_entry_url.grid(row=3, column=1, padx=10, pady=(0, 20), sticky="ew")
 
     def get_data(self) -> dict:
         """
@@ -165,13 +144,16 @@ class ApplicationFrame(CTkFrame):
         ex:
         {
             "path": "C:/Program Files/Google/Chrome/chrome.exe",
-            "urls": ["https://www.google.com", "https://www.youtube.com"]
+            "urls": ["https://www.python.org/", "https://customtkinter.tomschimansky.com/"]
         }
         """
         if self.app_path_SV is not None:
             data = {"path": self.app_path_SV.get()}
-            if self.tab_url is not None:
-                data["urls"] = [url.get() for url in self.url_SV]
+            if self.entry_url is not None:
+                data["urls"] = self.entry_url.get("0.0", "end").split("\n")
+                for url in data["urls"]:
+                    if url == "":
+                        data["urls"].remove(url)
             return data
 
     def open_explorer(self):
@@ -200,16 +182,6 @@ class ApplicationFrame(CTkFrame):
         self.update_color(app_path)
         CHANGE_MADE = True
 
-    def update_url_label(self, *args):
-        """
-        Update the label and the entry color with the entry value
-        """
-        global CHANGE_MADE
-        for i in range(len(self.entry_url)):
-            url_path = self.url_SV[i].get()
-            self.label_url[i].configure(text=get_site_name(url_path))
-        CHANGE_MADE = True
-
     def update_color(self, app_path):
         """
         Update the label and the entry color with the entry value.
@@ -223,14 +195,18 @@ class ApplicationFrame(CTkFrame):
             self.label_path_app.configure(text_color="#000")
             self.entry_path_app.configure(text_color="#FFFFFF")
 
-    def delete_url(self, index):
-        answer: bool = askyesno(title="Confirmation", message="Delete this url ?\n" + self.url_SV[index].get())
-        if answer:
-            self.url_SV.pop(index)
-            self.label_url.pop(index)
-            self.btn_delete_url.pop(index)
-            self.entry_url.pop(index)
-        self.show_urls()
+    def add_url(self, from_user=False):
+        """
+        Add the text area to enter an url
+        :param from_user: if the user add an url or if it's from the template
+        :return:
+        """
+        if from_user:
+            self.add_entry_url.grid_remove()
+
+        self.entry_url = CTkTextbox(self, text_color="#FFF")
+        self.entry_url.insert("0.0", "\n".join(self.url_internet))
+        self.entry_url.grid(row=3, column=1, padx=10, pady=(0, 20), sticky="ew")
 
 
 def get_application_name(executable_path: str) -> str:
@@ -248,4 +224,6 @@ def get_site_name(url: str) -> str:
     :param url: the url of the site (ex: https://www.google.com)
     :return: the name of the site (ex: Google)
     """
+    if url == "":
+        return "-"
     return url.split("/")[2].split(".")[0].capitalize()
